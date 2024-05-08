f95dfbc71bec42ba2323a9eb42dfe4be9fe4b733
TaeHoon <taehoondev030@gamil.com>
2024-05-08 22:30:22 +0900

    Added profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,32 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing our APIView"""
 
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = { #password에 추가 속성 부여
+            'password': {
+            'write_only':True,
+            'style': {'input_type': 'password'}
+            }
+        }
+
+        def create(self, validated_data):
+            """Create and return a new user"""
+            user = models.UserProfile.objects.create_user(
+                email=validated_data['email'],
+                name=validated_data['name'],
+                password=validated_data['password']
+            )
+
+            return user
